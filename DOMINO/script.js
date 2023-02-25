227f5bb54cd137883e7b484335c117c4a411a91d
velibor771 <126099609+velibor771@users.noreply.github.com>
2023-02-25 15:55:36 +0100

    Add files via upload

---
+++ DOMINO/script.js
@@ -0,0 +1,11 @@
+let onama = document.getElementById('onama')
+onama.addEventListener('click', () => {
+   window.location.href = 'about.html'
+})
+let ocjene = document.getElementById('ocjene')
+ocjene.addEventListener('click', () => {
+   window.location.href = 'https://www.tripadvisor.com/Restaurant_Review-g2613475-d21285599-Reviews-Domino_cafe_restoran-Pale_Republika_Srpska.html'
+})
+let facebook = document.getElementById('facebook').addEventListener('click', () => {
+   window.location.href = 'https://www.facebook.com/dominopale/'
+})
